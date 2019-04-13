77874de2e07b0ec51d97bd713a6ce8893400e5d7
Jason Springer <jsnsprngr@gmail.com>
2019-04-13 15:45:51 -0400

    Edit styling for authors, add new fields to Author model.

--- catalog/models.py
+++ catalog/models.py
@@ -110,6 +110,8 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('died', null=True, blank=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the author")
+    image = models.CharField(max_length=500)
 
     class Meta:
         ordering = ['last_name', 'first_name']
